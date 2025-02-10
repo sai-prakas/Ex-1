d479d8d4483104e3a83aa5fa7a82c9a6dd046010
sai-prakas <95365943+sai-prakas@users.noreply.github.com>
2025-02-10 23:16:54 +0530

    added main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+// Simple Java program to print a message
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub! This file was added directly.");
+    }
+}
+// Simple Java program to print a message
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub! This file was added directly.");
+    }
+}

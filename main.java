5fae3b76da30cc612c56002fbbe3e86147f20f87
sai-prakas <95365943+sai-prakas@users.noreply.github.com>
2025-02-11 07:18:34 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,12 @@
-// Simple Java program to print a message
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub! This file was added directly.");
-    }
-}
-// Simple Java program to print a message
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, GitHub! This file was added directly.");
+        System.out.println("Hello, Jenkins!");
+
+        // Simple addition example
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }

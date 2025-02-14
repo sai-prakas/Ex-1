c02e88cc3b312c686b4cbfb0da99f004aaa22b72
sai-prakas <95365943+sai-prakas@users.noreply.github.com>
2025-02-14 12:44:35 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
-
-        // Simple addition example
-        int num1 = 10;
-        int num2 = 20;
-        int sum = num1 + num2;
-        
-        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("Hello from Jenkins!");
     }
 }

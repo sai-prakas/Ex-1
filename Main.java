0d3e0f1d32986f19fc9580e005bd8d108b0d9353
sai-prakas <95365943+sai-prakas@users.noreply.github.com>
2025-02-15 11:18:57 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!");
+        System.out.println("Hello, Jenkins!");
+
+        int num1 = 5;
+        int num2 = 4;
+        int product = multiply(num1, num2);
+
+        System.out.println("Product of " + num1 + " and " + num2 + " is: " + product);
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
     }
 }
+
